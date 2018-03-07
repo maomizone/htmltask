fe0d8695111fa4f6a7c7fd5c04eb9570bb13ca4c
maomizone <404857030@qq.com>
2018-03-07 15:18:41 +0800

    djy

--- task/js/common/app.js
+++ task/js/common/app.js
@@ -1,9 +1,19 @@
 /**
  * Created by Administrator on 2017/6/14 0014.
  */
+ 
+ window.alert = function (name) {
+    var iframe = document.createElement("IFRAME");
+    iframe.style.display = "none";
+    iframe.setAttribute("src", 'data:text/plain,');
+    document.documentElement.appendChild(iframe);
+    window.frames[0].window.alert(name);
+    iframe.parentNode.removeChild(iframe);
+}
+
 
 // 测试
-const BASE_URL = 'http://192.168.10.21:8080/Lock/inter_json/';
+const BASE_URL = 'http://192.168.10.201:8089/Lock/inter_json/';
 // 正式
 // const BASE_URL = 'http://www.5ycl.net:8000/Lock/inter_json/';
 
