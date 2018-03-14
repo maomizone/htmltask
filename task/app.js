79bd41b613ca1de4692df2099bcb010caac9786b
maomizone <404857030@qq.com>
2018-03-14 09:59:41 +0800

    djy

--- task/app.js
+++ task/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/6/14 0014.
  */
- 
+
  window.alert = function (name) {
     var iframe = document.createElement("IFRAME");
     iframe.style.display = "none";
@@ -13,9 +13,9 @@
 
 
 // 测试
-const BASE_URL = 'http://192.168.10.201:8089/Lock/inter_json/';
+// const BASE_URL = 'http://192.168.10.201:8089/Lock/inter_json/';
 // 正式
-// const BASE_URL = 'http://www.5ycl.net:8000/Lock/inter_json/';
+const BASE_URL = 'http://www.5ycl.net:8000/Lock/inter_json/';
 
 const NET_ERROR = "网络故障，请调整网络信号后重新尝试。";
 
